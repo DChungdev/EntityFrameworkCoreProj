cbe8e9b5a4e0d920f940aa0a3b9fca7d6f69fef5
Nguyen Duc Chung <nchung1020@gmail.com>
2024-07-23 21:02:04 +0700

    Create and Configure Entityframework

---
+++ FirstEntityProj/Proj1/SchoolDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj1
+{
+    public class SchoolDbContext : DbContext
+    {
+        //entities
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-AFRCPMNE\\SQLEXPRESS;Initial Catalog=SchoolDb;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
